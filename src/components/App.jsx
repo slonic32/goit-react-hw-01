b864b153f0e096f870f1b8b70d3a26b97b46b808
Artem Hordiyevskyy <artem2500@gmail.com>
2024-01-19 00:17:06 +0200

    homework #1

---
+++ src/components/App.jsx
@@ -0,0 +1,24 @@
+import Profile from "./Profile/Profile";
+import FriendList from "./FriendList/FriendList";
+import TransactionHistory from "./TransactionHistory/TransactionHistory";
+import userData from "./data/userData.json";
+import friends from "./data/friends.json";
+import items from "./data/items.json";
+
+export default function App() {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+
+      <FriendList friends={friends} />
+
+      <TransactionHistory items={items} />
+    </>
+  );
+}
---
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,14 @@
+import clsx from "clsx";
+import css from "./FriendListItem.module.css";
+
+export default function FriendListItem({ avatar, name, isOnline }) {
+  return (
+    <div className={css.item}>
+      <img className={css.avatar} src={avatar} alt="Avatar" width="48" />
+      <p className={css.name}>{name}</p>
+      <p className={clsx(css.status, isOnline && css.onLine)}>
+        {isOnline ? "Online" : "Offline"}
+      </p>
+    </div>
+  );
+}
